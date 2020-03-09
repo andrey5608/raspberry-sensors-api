3b186f406fbb01a9ae6584fcff332455da02cd7d
andrey5608 <andrey5608@gmail.com>
2020-03-09 21:01:50 +0300

    Added gitignore and format code file

--- measuringApi.py
+++ measuringApi.py
@@ -9,29 +9,34 @@ DHT_PIN = 4
 app = Flask(__name__)
 CORS(app)
 
+
 def getTempAndHumidity():
-        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-        if humidity is not None and temperature is not None:
-                return temperature, humidity
-        else:
-                print("Failed to retrieve data from DHT22")
-                return temperature, humidity
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    if humidity is not None and temperature is not None:
+        return temperature, humidity
+    else:
+        print("Failed to retrieve data from DHT22")
+        return temperature, humidity
+
 
 def getBoardTemp():
-        output = str(subprocess.check_output("/usr/bin/vcgencmd measure_temp", stderr=subprocess.STDOUT, shell=True))
-        temperature = re.search(r'[0-9]+\.[0-9]+', output)
-        return temperature.group()
+    output = str(subprocess.check_output("/usr/bin/vcgencmd measure_temp", stderr=subprocess.STDOUT, shell=True))
+    temperature = re.search(r'[0-9]+\.[0-9]+', output)
+    return temperature.group()
+
 
 def formatTempAndHumidity():
-        temperatureValue, humidityValue = getTempAndHumidity()
-        return {
+    temperatureValue, humidityValue = getTempAndHumidity()
+    return {
         'sensorsData':
-        {'temperature': temperatureValue, 'humidity': humidityValue, 'boardCpuTemp': getBoardTemp()}
-        }
+            {'temperature': temperatureValue, 'humidity': humidityValue, 'boardCpuTemp': getBoardTemp()}
+    }
+
 
 @app.route("/tempAndHumidity", methods=["GET"])
 def getTemperatureAndHumidity():
-        return jsonify(formatTempAndHumidity())
+    return jsonify(formatTempAndHumidity())
+
 
 if __name__ == '__main__':
-        app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080)
