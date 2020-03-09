c3d9184b29c2ef68507c1e0e861ea3c507808f3a
andrey5608 <andrey5608@gmail.com>
2020-03-09 20:51:17 +0300

    Update measuringApi.py

--- measuringApi.py
+++ measuringApi.py
@@ -1,6 +1,6 @@
+import subprocess
 from flask import Flask, jsonify
 from flask_cors import CORS
-from subprocess import Popen, PIPE
 import re
 import Adafruit_DHT
 
@@ -9,35 +9,29 @@ DHT_PIN = 4
 app = Flask(__name__)
 CORS(app)
 
+def getTempAndHumidity():
+        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+        if humidity is not None and temperature is not None:
+                return temperature, humidity
+        else:
+                print("Failed to retrieve data from DHT22")
+                return temperature, humidity
 
-def get_temp_and_humidity():
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-    if humidity is not None and temperature is not None:
-        return temperature, humidity
-    else:
-        print("Failed to retrieve data from DHT11")
-        return temperature, humidity
+def getBoardTemp():
+        output = str(subprocess.check_output("/usr/bin/vcgencmd measure_temp", stderr=subprocess.STDOUT, shell=True))
+        temperature = re.search(r'[0-9]+\.[0-9]+', output)
+        return temperature.group()
 
-
-def get_board_temp():
-    output = Popen("vcgencmd measure_temp | egrep -o '[0-9]+\.[0-9]+'", shell=True, stdout=PIPE)
-    output_result = str(output.communicate())
-    temperature = re.search(r'[0-9]+\.[0-9]+', output_result)
-    return float(temperature.group(0))
-
-
-def format_temp_and_humidity():
-    temperature_value, humidity_value = get_temp_and_humidity()
-    return {
+def formatTempAndHumidity():
+        temperatureValue, humidityValue = getTempAndHumidity()
+        return {
         'sensorsData':
-            {'temperature': temperature_value, 'humidity': humidity_value, 'boardCpuTemp': get_board_temp()}
-    }
-
+        {'temperature': temperatureValue, 'humidity': humidityValue, 'boardCpuTemp': getBoardTemp()}
+        }
 
 @app.route("/tempAndHumidity", methods=["GET"])
-def get_temperature_and_humidity():
-    return jsonify(format_temp_and_humidity())
-
+def getTemperatureAndHumidity():
+        return jsonify(formatTempAndHumidity())
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)
+        app.run(host="0.0.0.0", port=8080)
